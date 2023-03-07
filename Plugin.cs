420c861ca57c0c05e9182d2534c509a3830078d5
abbydiode <contact@abbydiode.com>
2023-03-07 22:22:04 +0100

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,31 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Configuration;
+using HarmonyLib;
+using MyGame;
+using MyGame.InputStatus;
+
+namespace ButtonPromptOverride;
+
+[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin {
+    public static ManualLogSource logger;
+    public static ConfigEntry<IconType> iconType;
+    public static ConfigEntry<bool> overrideEnabled;
+    private void Awake() {
+        logger = Logger;
+        Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} {PluginInfo.PLUGIN_VERSION} loaded!");
+        
+        overrideEnabled = Config.Bind("", "Enabled", true, "Whether to override the button prompts");
+        iconType = Config.Bind("", "IconType", IconType.PS4, "Which button prompts to force the game to use");
+
+        Logger.LogInfo(overrideEnabled.Value ? $"Button prompts overriden to {iconType.Value}" : "Button prompt override disabled");
+        
+        new Harmony(PluginInfo.PLUGIN_GUID).PatchAll();
+    }
+}
+
+[HarmonyPatch(typeof(InputPadRewired), "IconType", MethodType.Getter)]
+public class InputPadRewired_IconType {
+    public static IconType Postfix(IconType __result) => Plugin.overrideEnabled.Value ? Plugin.iconType.Value : __result;
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# Button Prompt Override for Katamari Damacy REROLL
+
+## Description
+
+When Steam Input is enabled regular ABXY prompts are shown instead of the expected PlayStation prompts. This mod lets you override the prompts with whichever you want to fix this problem.
+
+## Installation
+
+1. Install the x86 version of [BepInEx 5](https://docs.bepinex.dev/articles/user_guide/installation/index.html)
+2. Drag and drop `ButtonPromptOverride.dll` to the BepInEx plugins directory
